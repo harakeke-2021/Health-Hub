d606d10c1be69f23f420d671822b2f57143ac95f
wallace222 <54966219+wallace222@users.noreply.github.com>
2021-02-26 17:18:15 +1300

    partial setting up form

--- client/components/Status.jsx
+++ client/components/Status.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
-function Status () {
+function Status (props) {
   return (
     <>
       <h1>Here is your current progress</h1>
+      <h2>{props.cWeight}</h2>
+      <h2>{props.gWeight}</h2>
+      <h2>{props.mCalories}</h2>
+      <h2>{props.cCalories}</h2>
+      <h2>{props.setDaysTillGoal}</h2>
+      {/* display props */}
     </>
   )
 }
